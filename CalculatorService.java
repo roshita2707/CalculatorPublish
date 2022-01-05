e3724a666d9506e2d3b9342a776cdfd3ee87951d
roshitha2707 <72878563+roshita2707@users.noreply.github.com>
2022-01-05 12:07:26 +0530

    Add files via upload

---
+++ CalculatorService.java
@@ -0,0 +1,19 @@
+package Service;
+
+
+public class CalculatorService {
+	public double add(double a, double b) {
+		return a+b;
+	}
+	public double sub(double a, double b) {
+		return a-b;
+	}
+	public double div(double a, double b) {
+		return a/b;
+	}
+	public double mul(double a, double b) {
+		return a*b;
+	}
+
+
+}
